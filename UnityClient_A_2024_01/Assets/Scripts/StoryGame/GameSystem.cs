c1703742c3b2f2773a7cf3db1d81bc88af0fd24e
taejune0927 <taejune0927@gmail.com>
2024-04-09 10:43:25 +0900

    [feat] gamesystem 스킵 업데이트

--- UnityClient_A_2024_01/Assets/Scripts/StoryGame/GameSystem.cs
+++ UnityClient_A_2024_01/Assets/Scripts/StoryGame/GameSystem.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Text;
+using UnityEngine.UI;
+using TMPro;
 
 
 namespace STORYGAME
@@ -22,21 +24,113 @@ namespace STORYGAME
             {
                 gameSystem.ResetStoryModels();
             }
+
+            if(GUILayout.Button("Assing Text Component by name"))
+            {
+                //오브젝트 이름으로 Text 컴포넌트 찾기
+                GameObject textObject = GameObject.Find("StroyTextUI");
+                if (textObject != null)
+                {
+                    Text textComponent = textObject.GetComponent<Text>();
+                    if(textComponent != null)
+                    {
+                        //Text Componet 할당
+                        gameSystem.textComponent = textComponent;
+                        Debug.Log("Text Component assigned Successfully");
+                    }
+                }
+            }
         }
 
     }
 #endif
     public class GameSystem : MonoBehaviour
     {
-        public static GameSystem instance;
+        public static GameSystem instance;                  //간단한 싱글톤 화
+        public Text textComponent = null;
+
+        public float delay = 0.1f;
+        public string fullText;
+        public string currentText = "";
+
+        public enum GAMESTATE
+        {
+            STORYSHIW,
+            WAITSELECT,
+            STROTYEND,
+            ENDMODE
+        }
+
+        public GAMESTATE currentState;
+        public StoryTableObject[] storyModels;
+        public StoryTableObject currentModels;
+        public int currentStoryIndex;
+        public bool showStroy = false;
 
         private void Awake()
         {
             instance = this;
         }
 
+        public void Start()                     //게임 시작시
+        {
+            StartCoroutine(ShowText());           //텍스트를 보여준다,
+        }
 
-        public StoryTableObject[] storyModels;
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) StoryShow(1);                         //Q 키를 누르면 1번 스토리
+            if (Input.GetKeyDown(KeyCode.W)) StoryShow(2);                         //W 키를 누르면 2번 스토리
+            if (Input.GetKeyDown(KeyCode.E)) StoryShow(3);                         //E 키를 누르면 3번 스토리
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                delay = 0.0f;
+            }
+        }
+
+
+        public void StoryShow(int number)
+        {
+            if (!showStroy)
+            {
+                currentModels = FindStoryModel(number);                       //스토리 모델 번호를 찾아서
+                delay = 0.1f;
+                StartCoroutine(ShowText());                                   //루틴을 실행 시킨다
+            }
+            
+        }
+
+
+        StoryTableObject FindStoryModel(int number)                                //스토리 모델 번호로 찾는 함수
+        {
+            StoryTableObject tempStoryModels = null;                               //temp 미리 저장 해놓은 변수를 선언
+            for(int i = 0; i < storyModels.Length; i ++)
+            {
+                if(storyModels[i].storyNumber == number)
+                {
+                    tempStoryModels = storyModels[i];
+                    break;
+                }
+            }
+
+            return tempStoryModels;
+        }
+
+        IEnumerator ShowText()
+        {
+            showStroy = true;
+            for(int i = 0; i <= currentModels.storyText.Length; i++)
+            {
+                currentText = currentModels.storyText.Substring(0, i);
+                textComponent.text = currentText;
+                yield return new WaitForSeconds(delay);
+            }
+
+            yield return new WaitForSeconds(delay);
+            showStroy = false;
+        }
 
 #if UNITY_EDITOR
         [ContextMenu("Reset Stroy Models")]
